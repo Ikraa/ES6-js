b6a28f93c8f93b590f59970098f46fc3123e4994
Ikraa <munnisaikra750@gmail.com>
2021-08-24 21:55:42 +0600

    arrow function es6

---
+++ arrow-function.js
@@ -0,0 +1,50 @@
+// function add(num1, num2){
+//     sum=num1+num2;                   // Function Which we have used before in js:
+//     return sum;
+// };
+// const result= add(12, 12);
+// // console.log(result);
+
+
+// const add1= function add1(num1, num2){ 
+//     return num1+num2;                      // expression function:
+// }; 
+// const result1=add1(10, 15);         
+// // console.log(result1);
+
+
+// const add2= function(num1, num2){ 
+//     return num1+num2;                   // expression function with anonymous function
+// };
+// const result2=add2(2, 2);
+// // console.log(result2);
+
+//-----------------------------Arrow function---------------------------------------
+// without word of function creating a function of ES6 rules:
+// const add3=(num1, num2)=>num1+num2;
+// const sum=add3(3, 3);
+// console.log(sum);
+
+// const tenTimes=num=>num*10;      //  single parameter without bracket.
+// const result4=tenTimes(5);
+// console.log(result4);
+
+// const multiply=(num1, num2, num3)=>num1*num2*num3;
+// const result5=multiply(2,2,2);                    // multiply operation
+// console.log(result5);
+
+// const getWord=()=>"Hello World";
+// const word=getWord();                    // using anonymous function
+// console.log(word);
+
+// const doMath= (x, y) =>{             //multi line arrow-function
+//     add=x+y;
+//     diff=x-y;
+//     const output=add*diff;
+//     return output;
+// }
+// const total=doMath(4, 2);
+// console.log(total);
+
+
+
